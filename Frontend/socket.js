96826f637c527cc53073dca19720daa61432890e
Prynde <miika.stahl@student.hamk.fi>
2025-10-05 13:49:34 +0300

    Socket-con. complete, file upload via socket (untested).

--- Frontend/socket.js
+++ Frontend/socket.js
@@ -2,33 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import io from "socket.io-client";
 const socket = io.connect("https://lappis.mau-mooneye.ts.net", {transports: ['websocket']});
-const data = {
-      username: "test",
-      password: "test",
-    };
-const socketTest = () => {
-  alert("Status: " + socket.connected);
-  socket.emit("authentication", { ...data, register: true });
-};
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button onPress={socketTest} color="blue" size="large"
-        title="Test">
-          Test
-      </Button>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const socketTest = () => { // Test register
+  socket.emit("authentication", { username, password, register: true }
+)};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const socketTest2 = () => { // Test login
+  socket.emit("authentication",  { username, password } 
+)};
+
+const socketTest3 = () => { // Test login
+  socket.emit("logintest"
+)};
+
+socket.on('unauthorized', (err) => { // Show message if wrong username or password was given
+  alert("There was an error with the authentication: " + err.message);
 });
+
+socket.on('registered', (message) => { // Show message for succesfull registration
+  alert(message.message + " registered succesfully.");
+});
+
+socket.on('loggedIn', (message) => { // Show message for succesfull registration
+  alert(message.message);
+});
+
+socket.on('testok', (message) => { // Show message for succesfull registration
+  alert("Test ok");
+});
+
+function upload(files) {
+  socket.emit("upload", files[0], (status) => {
+    alert(status);
+  });
+};
