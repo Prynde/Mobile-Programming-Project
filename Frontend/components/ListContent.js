16a77d371b64caaa2a93fa1dd005ebbcbe280468
juhonevalainen <97619711+juhonevalainen@users.noreply.github.com>
2025-10-10 21:08:53 +0300

    Temporarily saved lists messages and titles can be edited.

--- Frontend/components/ListContent.js
+++ Frontend/components/ListContent.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function ListContent({setVisibility, selectedList}) { 
     console.log(selectedList)
+    const [title, setTitle] = useState("")
+    const [message, setMessage] = useState("") // Message that is shown.
+    const [edited, setEdited] = useState(false) // If user have edited message in any way this is changed to true.
+    // TextInput is initialized with useEffect on first render.
+    useEffect(() => {
+        setTitle(selectedList.content[0].title)
+        setMessage(selectedList.content[0].message)
+    }, []);
 
     const handleProfile = () => {
+        handleSaving()
         setVisibility(false)
     }
 
+    const handleTitle = (props) => {
+        setEdited(true) // Called every time something is pressee, find a better way?
+        setTitle(props)
+        handleSaving()
+    }
+
+    const handleMessage = (props) => {
+        setEdited(true) // Called every time something is pressee, find a better way?
+        setMessage(props)
+        handleSaving()
+    }
+
+    // Saves title and message to the object and updates date if either has been edited.
+    const handleSaving = () => {
+        if (edited) {
+            selectedList.content[0].title = title;
+            selectedList.content[0].message = message;
+            const newDate = new Date();
+            selectedList.content[0].date = newDate.toISOString();
+        }
+    }
+    
     return(
         <View style={styles.container}>
             <View style={styles.subContainer}>
-                <Text style={styles.profileHeader}>Otsikko: {selectedList.content[0].title}</Text>
-                <Text style={styles.profileHeader}>Viesti: {selectedList.content[0].message}</Text>
+                <TextInput style={{ width: "100%", borderWidth: 1}} onChangeText={handleTitle} value={title} />
+                <TextInput style={{ width: "100%", height: "100%", borderWidth: 1}} multiline autoFocus={true} onChangeText={handleMessage} numberOfLines={10} value={message} />
             </View>
             <View style={styles.subContainer}>
                 <TouchableOpacity style={styles.buttonInput} onPress={handleProfile}>
